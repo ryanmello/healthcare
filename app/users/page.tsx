2ccb4726bc85bbcc1275fe28cf5211597fb7716b
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-03-13 10:10:43 -0700

    Your commit message

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,7 +3,7 @@ import { getAllDatabaseUsers } from "../actions/get-all-db-users";
 const Users = async () => {
   const users = await getAllDatabaseUsers();
   return (
-    <div>
+    <div className="flex flex-col">
       {users.map((user) => (
         <p key={user.id}>{user.email}</p>
       ))}

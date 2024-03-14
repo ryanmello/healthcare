58dfc6b4954de033ccf13a3d88c81b948d9afb6e
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-03-14 12:28:09 -0700

    appointment, fav, create

---
+++ app/dashboard/components/CreateAppointmentModal.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+
+const CreateAppointmentModal = () => {
+  return (
+    <Button size="sm">
+      <PlusIcon size={16} />
+    </Button>
+  );
+};
+
+export default CreateAppointmentModal;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import CreateAppointmentModal from "./components/CreateAppointmentModal";
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
+    <MaxWidthWrapper className="flex mt-4">
+      <div className="w-1/3">
+        <div className="px-2">
+          <div className="flex items-center justify-between my-4">
+            <h2 className="font-semibold">Upcoming appointments</h2>
+            <CreateAppointmentModal />
+          </div>
+          <div className="flex flex-col space-y-2">
+            <div className="w-full h-24 border-2 border-slate-700 rounded-md"></div>
+            <div className="w-full h-24 border-2 border-slate-700 rounded-md"></div>
+            <div className="w-full h-24 border-2 border-slate-700 rounded-md"></div>
+          </div>
+        </div>
+      </div>
+    </MaxWidthWrapper>
+  );
+};
 
-export default Dashboard
+export default Dashboard;

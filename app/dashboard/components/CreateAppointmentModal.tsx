5836bede0d9aaff9c5f8e140da9c97c769ce8075
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-03-14 13:08:51 -0700

    modal

--- app/dashboard/components/CreateAppointmentModal.tsx
+++ app/dashboard/components/CreateAppointmentModal.tsx
@@ -1,11 +1,32 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { PlusIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const CreateAppointmentModal = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  
   return (
-    <Button size="sm">
-      <PlusIcon size={16} />
-    </Button>
+    <Dialog>
+      <DialogTrigger>
+        <Button size="sm">
+          <PlusIcon size={16} />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <p>HELLO</p>
+      </DialogContent>
+    </Dialog>
   );
 };
 

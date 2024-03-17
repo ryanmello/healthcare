78f2191c4f5f1de05f798a728a6f254181598577
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-03-17 10:46:25 -0700

    dropdown menu

--- components/sidebar/SidebarBody.tsx
+++ components/sidebar/SidebarBody.tsx
@@ -8,6 +8,8 @@ import {
   HomeIcon,
   LayoutDashboard,
   MoreVertical,
+  Settings,
+  User as UserIcon,
 } from "lucide-react";
 import Image from "next/image";
 import logo from "@/public/logo.svg";
@@ -15,12 +17,19 @@ import { useState } from "react";
 import { cn } from "@/lib/utils";
 import SidebarItem from "./SidebarItem";
 import { User } from "@prisma/client";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
-const SidebarBody = ({
-  user
-}: {
-  user: User | null;
-}) => {
+const SidebarBody = ({ user }: { user: User | null }) => {
   const [expanded, setExpanded] = useState(true);
 
   return (
@@ -36,7 +45,13 @@ const SidebarBody = ({
           width={32}
           height={32}
         />
-        <button onClick={() => setExpanded(!expanded)} className={cn("flex items-center justify-center", !expanded && "w-full")}>
+        <button
+          onClick={() => setExpanded(!expanded)}
+          className={cn(
+            "flex items-center justify-center",
+            !expanded && "w-full"
+          )}
+        >
           {expanded ? <ChevronFirst /> : <ChevronLast />}
         </button>
       </div>
@@ -71,15 +86,40 @@ const SidebarBody = ({
               width={100}
             />
           )}
-          <div className={cn("flex flex-col justify-center leading-4 cursor-default ml-2", !expanded && "hidden")}>
-            <h4 className="font-semibold">{user?.firstName} {user?.lastName}</h4>
+          <div
+            className={cn(
+              "flex flex-col justify-center leading-4 cursor-default ml-2",
+              !expanded && "hidden"
+            )}
+          >
+            <h4 className="font-semibold">
+              {user?.firstName} {user?.lastName}
+            </h4>
             <span className="text-xs text-slate-500">{user?.email}</span>
           </div>
         </div>
-        <MoreVertical
-          size={20}
-          className={cn("cursor-pointer", !expanded && "hidden")}
-        />
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MoreVertical
+              size={20}
+              className={cn("cursor-pointer", !expanded && "hidden")}
+            />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-48">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="cursor-pointer">
+                <UserIcon className="mr-2 h-4 w-4" />
+                <span>Profile</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer">
+                <Settings className="mr-2 h-4 w-4" />
+                <span>Settings</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </>
   );

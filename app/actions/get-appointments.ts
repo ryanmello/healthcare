3191d7583315eef49745822b13badb3de3f96d78
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-04-07 19:53:51 -0700

    get archived false

--- app/actions/get-appointments.ts
+++ app/actions/get-appointments.ts
@@ -3,6 +3,9 @@ import db from "@/lib/db";
 export const getAppointments = async () => {
   try {
     const appointments = await db.appointment.findMany({
+      where: {
+        archived: false,
+      },
       include: {
         patient: true,
         user: true,

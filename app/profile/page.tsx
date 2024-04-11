8e907aef086d25dde9664d30d66be2c875ed564b
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-04-11 14:19:53 -0700

    heading

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import EditNameForm from './components/EditNameForm'
-import { getUser } from '../actions/get-user'
-import { auth } from '@clerk/nextjs';
+import React from "react";
+import EditNameForm from "./components/EditNameForm";
+import { getUser } from "../actions/get-user";
+import { auth } from "@clerk/nextjs";
 
 const Profile = async () => {
   const { userId } = auth();
   const user = await getUser({ userId });
   return (
     <div>
-      {user != null && (
-    <EditNameForm user={user}/>
-  )}
+      <p>Edit</p>
+      {user != null && <EditNameForm user={user} />}
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;

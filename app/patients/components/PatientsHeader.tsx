6010b7071df5a38c16464ddbb1bf2427375fe586
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-04-15 11:20:34 -0700

    add patient in patiients page

---
+++ app/patients/components/PatientsHeader.tsx
@@ -0,0 +1,163 @@
+"use client";
+
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { PlusIcon } from "lucide-react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  firstName: z.string().min(2).max(50),
+  lastName: z.string().min(2).max(50),
+  email: z.string().min(2),
+  phone: z.string().min(2),
+  dob: z.string().min(2),
+  gender: z.string().min(2),
+  address: z.string().min(2),
+});
+
+const PatientsHeader = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      dob: "",
+      gender: "",
+      address: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.post("/api/patient/create", values);
+      toast.success("Patient created");
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+  return (
+    <div className="flex items-center justify-between my-6">
+      <h1 className="font-semibold text-lg">Patients Table</h1>
+      <Dialog>
+        <DialogTrigger>
+          <div className="bg-white text-black px-4 py-2 rounded-md flex items-center space-x-2">
+            <p className="font-bold text-sm">Add Patient</p>
+            <PlusIcon size={16} />
+          </div>
+        </DialogTrigger>
+        <DialogContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>First name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="First name" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Last name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Last name" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Phone number</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Phone number" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="dob"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Date of birth</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Date of birth" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="gender"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Gender</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Gender" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Address</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Address" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <DialogTrigger className="w-full">
+                <Button type="submit" className="w-full">
+                  Submit
+                </Button>
+              </DialogTrigger>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default PatientsHeader;
--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -1,24 +1,15 @@
-import { ColumnDef } from "@tanstack/react-table";
 import { getPatients } from "../actions/get-patients";
 import { PatientsTable } from "./components/PatientsTable";
-import { Patient } from "@prisma/client";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Button } from "@/components/ui/button";
-import { PlusIcon } from "lucide-react";
 import { columns } from "./components/PatientsColumns";
+import PatientsHeader from "./components/PatientsHeader";
 
 const Patients = async () => {
   const patients = await getPatients();
 
   return (
     <MaxWidthWrapper>
-      <div className="flex items-center justify-between my-6">
-        <h1 className="font-semibold text-lg">Patients Table</h1>
-        <Button className="flex items-center space-x-2">
-          <p className="font-bold text-sm">Add Patient</p>
-          <PlusIcon size={16} />
-        </Button>
-      </div>
+      <PatientsHeader />
       <PatientsTable columns={columns} data={patients} />
     </MaxWidthWrapper>
   );

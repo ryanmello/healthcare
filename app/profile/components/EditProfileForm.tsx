b83d13156df38c513234bbcb8dcb6dc3f407c297
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2024-04-22 12:51:33 -0700

    dropdown roles

--- app/profile/components/EditProfileForm.tsx
+++ app/profile/components/EditProfileForm.tsx
@@ -16,6 +16,20 @@ import { toast } from "sonner";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { User } from "@prisma/client";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface EditProfileFormProps {
   user: User;
@@ -29,6 +43,14 @@ const formSchema = z.object({
   phone: z.string().min(1, "Phone number is required"),
   role: z.string().min(1, "Role is required"),
 });
+
+const roles = [
+  { label: "Doctor", value: "Doctor" },
+  { label: "Physician Assistant", value: "Physician Assistant" },
+  { label: "Nurse", value: "Nurse" },
+  { label: "Nurse Practitioner", value: "Nurse Practitioner" },
+  { label: "Other", value: "user" },
+] as const;
 
 type FormValues = z.infer<typeof formSchema>;
 
@@ -107,14 +129,51 @@ const EditProfileForm: React.FC<EditProfileFormProps> = ({ user }) => {
           )}
         />
         <FormField
-          control={form.control} // would like this in a dropdown menu format instead, but this will do for now
+          control={form.control}
           name="role"
           render={({ field }) => (
-            <FormItem>
+            <FormItem className="flex flex-col">
               <FormLabel>Role</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button variant="outline" role="combobox">
+                      {field.value
+                        ? roles.find((role) => role.value === field.value)
+                            ?.label
+                        : "Select role"}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="p-0">
+                  <Command>
+                    <CommandInput placeholder="Search roles..." />
+                    <CommandEmpty>No role found.</CommandEmpty>
+                    <CommandGroup>
+                      {roles.map((role) => (
+                        <CommandItem
+                          value={role.label}
+                          key={role.value}
+                          onSelect={() => {
+                            form.setValue("role", role.value);
+                          }}
+                        >
+                          <Check
+                            className={cn(
+                              "mr-2 h-4 w-4",
+                              role.value === field.value
+                                ? "opacity-100"
+                                : "opacity-0"
+                            )}
+                          />
+                          {role.label}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </Command>
+                </PopoverContent>
+              </Popover>
             </FormItem>
           )}
         />
